6c233686d90b9eeefef3228253d6141debb2b7af
Nithin Vadakken...OR NOT <nitvad21@bergen.org>
2018-05-31 14:04:39 -0400

    snt

--- Development_Project/DevelopJS.js
+++ Development_Project/DevelopJS.js
@@ -4,3 +4,42 @@ canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 
 ctx = canvas.getContext('2d');
+
+var rightPressed = false;
+var leftPressed = false;
+var upPressed = false;
+var downPressed = false;
+
+document.addEventListener("keydown", keyDownHandler, false);
+document.addEventListener("keyup", keyUpHandler, false);
+
+function keyDownHandler(e) {
+    if ((((e.keyCode === 91) || (e.keyCode === 17)) && (e.keyCode === 16) && (e.keyCode === 67)) || (e.keyCode === 123)) {
+        return false;
+    }
+
+    if (e.keyCode === 68 || e.keyCode === 39) {
+        rightPressed = true;
+    } else if (e.keyCode === 65 || e.keyCode === 37) {
+        leftPressed = true;
+    } else if (e.keyCode === 87 || e.keyCode === 38) {
+        upPressed = true;
+    } else if (e.keyCode === 83 || e.keyCode === 40) {
+        downPressed = true;
+    }
+}
+
+function keyUpHandler(e) {
+    if (e.keyCode === 68 || e.keyCode === 39) {
+        rightPressed = false;
+    } else if (e.keyCode === 65 || e.keyCode === 37) {
+        leftPressed = false;
+    } else if (e.keyCode === 87 || e.keyCode === 38) {
+        upPressed = false;
+    } else if (e.keyCode === 83 || e.keyCode === 40) {
+        downPressed = false;
+    }
+}
+
+
+
