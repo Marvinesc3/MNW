18363daaf8a3973660ae9267730f5c3c65cf25a0
wikto <wiktorlidwin@gmail.com>
2018-06-07 10:40:26 -0400

    PYKE

--- Development_Project/DevelopJS.js
+++ Development_Project/DevelopJS.js
@@ -1,6 +1,7 @@
 var canvas = document.querySelector('canvas');
 var ship = document.getElementById("ship");
 var cannon = document.getElementById('cannon');
+var pyke = document.getElementById('pyke');
 
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
@@ -135,6 +136,8 @@ function Make_Bullets() {
     d = new Date();
     if(mousePos === true && shot +attack_speed< d.getTime()){
         Bullet(x+40,y+100, 3,arc2);
+        arc3 = 180 -arc2;
+        Bullet(x+40,y+100, 3,arc3);
         d = new Date();
         shot = d.getTime();
 
@@ -150,9 +153,41 @@ function MouseUp(TorF) {
         mousePos = false;
     }
 }
+pyke_x = Math.random() * (canvas.width - 100 * 2) + 100;
+pyke_y = Math.random() * (canvas.height - 200 * 2) + 200;
+repaint_pyke = true;
+function is_pyke_dead() {
+    for(i = 0; i< xPoslist.length;i++){
+
+        if(pyke_x+50> xPoslist[i]&& pyke_x<xPoslist[i] && pyke_y+50> yPoslist[i]&& pyke_y<yPoslist[i]){
+            pyke_x = Math.random() * (canvas.width - 100 * 2) + 100;
+            pyke_y = Math.random() * (canvas.height - 200 * 2) + 200;
+
+
+        }
+    }
+}
+
 function drawCharacter() {
     ctx.drawImage(ship, x, y, 100, 200);
-
+    if (repaint_pyke === true) {
+        if (x - pyke_x > 0) {
+            pyke_x += .3;
+        }
+        if (x - pyke_x < 0) {
+            pyke_x -= .3;
+        }
+        if ((y + 100) - pyke_y > 0) {
+            pyke_y += .3;
+        }
+        if ((y + 100) - pyke_y < 0) {
+            pyke_y -= .3;
+        }
+    }
+    is_pyke_dead();
+    if (repaint_pyke === true) {
+        ctx.drawImage(pyke, pyke_x, pyke_y, 50, 50);
+    }
     if (rightPressed) {
         rotation -= 15;
     }
