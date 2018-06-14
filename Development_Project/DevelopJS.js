c22b3c362292ab76808a82d89b6ad32292c00f67
wikto <wiktorlidwin@gmail.com>
2018-06-14 12:52:24 -0400

    enemy

--- Development_Project/DevelopJS.js
+++ Development_Project/DevelopJS.js
@@ -61,15 +61,175 @@ var speedlist =[];
 var arclist = [];
 var xPoslist = [];
 var yPoslist = [];
+var lifetimelist = [];
+var currenttimelist = [];
 
-function Bullet(xPos, yPos,speed,arc) {
+function Bullet(xPos, yPos,speed,arc, lifetime,currenttime) {
     speedlist.push(speed);
     arclist.push(arc);
 
     xPoslist.push(xPos);
     yPoslist.push(yPos);
+    lifetimelist.push(lifetime);
+    currenttimelist.push(currenttime);
 }
 
+var enemyXList = [];
+var enemyYList = [];
+var enemyWidthList = [];
+var enemyHeightList = [];
+var enemySpeedList = [];
+var enemyHealthList = [];
+var enemyImgList = [];
+var enemyAngList = [];
+var Baron1 = document.getElementById('Baron');
+
+ennemy_try1 = new Create_enemy(Baron1, 300,300, 2, 100,200,100,90);
+
+
+
+
+function Create_enemy(image,x,y,speed,width,height,health,ang) {
+    this.width = width;
+    this.height = height;
+    this.speed = speed;
+    this.health = health;
+    this.x = x;
+    this.y = y;
+    this.image = image;
+    this.ang = ang;
+
+    enemyAngList.push(this.ang);
+    enemyHealthList.push(this.health);
+    enemyHeightList.push(this.height);
+    enemyWidthList.push(this.width);
+    enemyXList.push(this.x);
+    enemyYList.push(this.y);
+    enemyImgList.push(this.image);
+    enemySpeedList.push(this.speed);
+
+    ctx.drawImage(this.image, this.x, this.y, this.width, this.height);
+
+    mapx = canvas.width / 2 - enemyXList[i];
+    mapy = canvas.height / 2 - enemyYList[i];
+
+
+
+    ctx.drawImage(map, 0, 0, mapWidth, mapHeight, mapx, mapy, mapWidth * 4, mapHeight * 4);
+
+
+
+}
+
+var enemies= {
+    rotator: function (x, y, ang, i) {
+
+
+
+            var rad = ang * Math.PI / 180;
+
+
+            ctx.translate(canvas.width/2-50 + enemyWidthList[i] / 2, canvas.height/2-100 + enemyHeightList[i] / 2);
+
+            ctx.rotate(rad);
+
+            ctx.drawImage(enemyImgList[i],enemyWidthList[i] / 2 * (-1),enemyHeightList[i] / 2 * (-1),enemyWidthList[i],enemyHeightList[i]);
+            ctx.rotate(rad * (-1));
+
+            ctx.translate((canvas.width/2-50 + enemyWidthList[i] / 2) * (-1), (canvas.height/2-100 + enemyHeightList[i] / 2) * (-1));
+
+
+
+
+    },
+    draw:function () {
+
+        ctx.clearRect(0,0, canvas.width,canvas.height);
+
+        for(i = 0; i<enemyXList.length; i++){
+
+
+
+            for(listPos = 0; listPos< xPoslist.length; listPos++) {
+                if(currenttimelist[listPos]<lifetimelist[listPos]) {
+                    x2 = xPoslist[listPos]+mapx;
+                    y2 = yPoslist[listPos]+mapy;
+                    yPoslist[listPos] -= (Math.sin(arclist[listPos] * Math.PI / 180.0)) * speedlist[listPos];
+                    xPoslist[listPos] += (Math.cos(arclist[listPos] * Math.PI / 180.0)) * speedlist[listPos];
+
+
+
+
+                    //ctx.drawImage(Baron, this.x2,this.y2);
+
+                    ctx.drawImage(cannon, x2, y2 ,20, 20);
+
+                    currenttimelist[listPos]+=.1
+                }
+            }
+
+            for(z = 0; z<enemyYList.length; z++){
+
+                    var subx = enemyXList[z]+mapx;
+                    var suby = enemyXList[z]+mapy;
+
+                    enemies.rotator(subx, suby, enemyAngList[z], i);
+                    enemies.move();
+
+
+
+            }
+            ctx.fillStyle = "black";
+            ctx.fillText(enemyXList[0]+" , "+enemyYList[0]+"   "+ mapx+" , "+mapy+"   " +subx+ " , "+suby+"   ", 0 , 100);
+        }
+
+
+       },
+    move: function() {
+
+
+
+
+
+        for(i = 0; i <enemyYList.length;i++){
+        if (upPressed) {
+            if (enemyYList[i] - enemySpeedList[i] * Math.sin((enemyAngList[i]+90) * Math.PI / 180) > 0 && enemyXList[i] - enemySpeedList[i] * Math.cos((enemyAngList[i]+90) * Math.PI / 180) > 0) {
+                enemyXList[i] -= enemySpeedList[i] * Math.cos((enemyAngList[i]+90) * Math.PI / 180);
+                enemyYList[i] -= enemySpeedList[i] * Math.sin((enemyAngList[i]+90) * Math.PI / 180);
+
+
+
+            }
+            else {
+                enemyXList[i] += enemySpeedList[i] * Math.cos((enemyAngList[i]+90) * Math.PI / 180);
+                enemyYList[i] += enemySpeedList[i] * Math.sin((enemyAngList[i]+90) * Math.PI / 180);
+
+
+            }
+        }
+        if (downPressed) {
+            if (this.y + this.speed * Math.sin((this.rotation+90) * Math.PI / 180)< mapHeight && this.x+this.speed * Math.cos((this.rotation+90) * Math.PI / 180) < mapWidth) {
+                enemyXList[i] -= enemySpeedList[i] * Math.cos((enemyAngList[i]+90) * Math.PI / 180);
+                enemyYList[i] -= enemySpeedList[i] * Math.sin((enemyAngList[i]+90) * Math.PI / 180);
+
+
+            }
+            else {
+                enemyXList[i] += enemySpeedList[i] * Math.cos((enemyAngList[i]+90) * Math.PI / 180);
+                enemyYList[i] += enemySpeedList[i] * Math.sin((enemyAngList[i]+90) * Math.PI / 180);
+
+            }
+        }
+
+        if (rightPressed && i === 0) {
+            enemyAngList[i]++;
+        }
+        if (leftPressed&& i === 0) {
+            enemyAngList[i]--;
+        }
+    }}
+
+};
 
 
 
@@ -77,6 +237,8 @@ var map = document.getElementById('map');
 var mapWidth = 780;
 var mapHeight = 430;
 var player = {
+    alpha: 1000,
+    omega: 500,
     x: Math.random() * (mapWidth - 100 * 2) + 100,
     y: Math.random() * (mapHeight - 200 * 2) + 200,
     dx: Math.random() * (canvas.width - 100 * 2) + 100,
@@ -89,12 +251,18 @@ var player = {
     health: 100,
     x1: canvas.width/2,
     y1: canvas.height/2,
-
+    x2:0,
+    y2:0,
+    numtest:0,
+    ax: 200,
+    ay: 200,
 
     rotator: function() {
         ctx.clearRect(0,0, canvas.width,canvas.height);
         var x = canvas.width/2 - player.x;
         var y = canvas.height/2 - player.y;
+
+        //enemies.draw();
 
         var listPos;
         this.x1 = x;
@@ -105,14 +273,27 @@ var player = {
 
 
         ctx.drawImage(map, 0, 0, mapWidth, mapHeight, x, y, mapWidth*4, mapHeight*4);
+
+        this.x2 = this.ax+x;
+        this.y2=this.ay+y;
+        this.numtest +=1;
+
+        this.ax -=1;
+        //ctx.drawImage(Baron, this.x2,this.y2);
+
+
+
         ctx.drawImage(pyke,0, 0, pyke.width, pyke.height, this.dx, this.dy, pyke.width*2, pyke.height*2);
 
-        for(listPos = 0; listPos< xPoslist.length; listPos++) {
-            ctx.drawImage(cannon, xPoslist[listPos], yPoslist[listPos], 20, 20);
-            yPoslist[listPos] -= (Math.sin(arclist[listPos] * Math.PI / 180.0))*speedlist[listPos];
-            xPoslist[listPos] += (Math.cos(arclist[listPos] * Math.PI / 180.0))*speedlist[listPos];
-        }
 
+        for(listPos = 0; listPos< xPoslist.length; listPos++) {
+            if(currenttimelist[listPos]<lifetimelist[listPos]) {
+                ctx.drawImage(cannon, xPoslist[listPos], yPoslist[listPos], 20, 20);
+                yPoslist[listPos] -= (Math.sin(arclist[listPos] * Math.PI / 180.0)) * speedlist[listPos];
+                xPoslist[listPos] += (Math.cos(arclist[listPos] * Math.PI / 180.0)) * speedlist[listPos];
+                currenttimelist[listPos]+=.1
+            }
+        }
         var rad = this.rotation * Math.PI / 180;
 
 
@@ -125,10 +306,10 @@ var player = {
 
         ctx.translate((canvas.width/2-50 + this.width / 2) * (-1), (canvas.height/2-100 + this.height / 2) * (-1));
         Make_Bullets();
-        a = this.dx-this.x;
-        ctx.fillStyle = "black";
-        ctx.fillText(this.x+" , "+this.y+"   "+ x+" , "+y+"   " +this.dx+ " , "+this.dy+"   "+a, 0 , 100);
 
+
+        //ctx.fillStyle = "black";
+        //ctx.fillText(this.x+" , "+this.y+"   "+ x+" , "+y+"   " +this.dx+ " , "+this.dy+"   "+a, 0 , 100);
 
     },
     move: function() {
@@ -138,21 +319,26 @@ var player = {
                 this.y -= this.speed * Math.sin((this.rotation+90) * Math.PI / 180);
                 this.dx += this.speed * Math.cos((this.rotation+90) * Math.PI / 180);
                 this.dy += this.speed * Math.sin((this.rotation+90) * Math.PI / 180);
-
+                this.alpha += this.speed * Math.cos((this.rotation+90) * Math.PI / 180);
+                this.omega += this.speed * Math.sin((this.rotation+90) * Math.PI / 180);
             }
             else {
                 this.x += this.speed * Math.cos((this.rotation+90) * Math.PI / 180);
                 this.y += this.speed * Math.sin((this.rotation+90) * Math.PI / 180);
                 this.dx -= this.speed * Math.cos((this.rotation+90) * Math.PI / 180);
                 this.dy -= this.speed * Math.sin((this.rotation+90) * Math.PI / 180);
+                this.alpha -= this.speed * Math.cos((this.rotation+90) * Math.PI / 180);
+                this.omega -= this.speed * Math.sin((this.rotation+90) * Math.PI / 180);
             }
         }
         if (downPressed) {
-            if (this.y + this.speed * Math.sin((this.rotation+90) * Math.PI / 180)< canvas.height && this.x+this.speed * Math.cos((this.rotation+90) * Math.PI / 180) < canvas.width) {
+            if (this.y + this.speed * Math.sin((this.rotation+90) * Math.PI / 180)< mapHeight && this.x+this.speed * Math.cos((this.rotation+90) * Math.PI / 180) < mapWidth) {
                 this.x += this.speed * Math.cos((this.rotation+90) * Math.PI / 180);
                 this.y += this.speed * Math.sin((this.rotation+90) * Math.PI / 180);
                 this.dx -= this.speed * Math.cos((this.rotation+90) * Math.PI / 180);
                 this.dy -= this.speed * Math.sin((this.rotation+90) * Math.PI / 180);
+                this.alpha -= this.speed * Math.cos((this.rotation+90) * Math.PI / 180);
+                this.omega -= this.speed * Math.sin((this.rotation+90) * Math.PI / 180);
 
             }
             else {
@@ -160,6 +346,8 @@ var player = {
                 this.y -= this.speed * Math.sin((this.rotation+90) * Math.PI / 180);
                 this.dx += this.speed * Math.cos((this.rotation+90) * Math.PI / 180);
                 this.dy += this.speed * Math.sin((this.rotation+90) * Math.PI / 180);
+                this.alpha += this.speed * Math.cos((this.rotation+90) * Math.PI / 180);
+                this.omega += this.speed * Math.sin((this.rotation+90) * Math.PI / 180);
             }
         }
 
@@ -189,13 +377,15 @@ var player = {
     },
 
     update: function() {
-        ctx.drawImage(pyke, this.dx, this.dy);
-        ctx.drawImage(this.image, this.x, this.y, this.width, this.height);
+
+
+
+
         this.rotator();
 
     }
 };
-
+var Baron = document.getElementById('Baron');
 var enemyArray = [];
 
 
@@ -251,9 +441,8 @@ function Make_Bullets() {
 
     d = new Date();
     if(mousePos === true && shot + attack_speed< d.getTime()){
-        Bullet(canvas.width/2,canvas.height/2, 3,arc2);
-        arc3 = player.rotation+180 -arc2;
-        Bullet(canvas.width/2,canvas.height/2 , 3,arc3);
+        Bullet(enemyXList[0],enemyYList[0], 3,arc2,20,0);
+
         d = new Date();
         shot = d.getTime();
 
@@ -273,7 +462,8 @@ function MouseUp(TorF) {
 
 
 function drawCharacter() {
-    player.update();}
+    player.update();
+    }
 
 
 function drawGame() {
@@ -281,7 +471,8 @@ function drawGame() {
     requestAnimationFrame(drawGame);
     Make_Bullets();
     drawCharacter();
-    player.move();
+    //player.move();
+    enemies.draw();
 
     drawHealthBar();
     updateEnemies();
@@ -292,3 +483,4 @@ function drawGame() {
 }
 
 drawGame();
+
